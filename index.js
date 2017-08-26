64e89af9d929cdb037ada837a9e299401b6f6ea1
movil-PC\movil <jorgelive2016@gmail.com>
2017-08-26 09:11:36 -0400

    jorge 123

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var server = require('http').Server(app);
 
 app.get('/', function(req,res){
-      res.status(200).send({'parametro': 'te amo ibeth'});
+      res.status(200).send({'parametro': 'jorge'});
 });
 server.listen(8080, function(){
   console.log("servidor corriedo 8080");
